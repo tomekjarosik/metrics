754fe3293dcd95c61be1820f514e195e645e5423
Tomasz Jarosik <tomek.jarosik@gmail.com>
2016-05-08 09:57:29 +0100

    Rest of the files from the tutorial

---
+++ app/views.py
@@ -0,0 +1,33 @@
+from app import app
+from flask import render_template
+from flask import flash, redirect
+from .forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Tomek'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/charts')
+def charts_demo():
+    # fake chart
+    #user = {'nickname': 'Tomek'}  # fake user
+    return render_template('charts.html')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
